caaee92b1d54001e28286456b65ce2a1c68c7dd7
isabellanina <is472808@ucf.edu>
2025-01-13 11:43:40 -0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 11:44:28 2025
+
+@author: isabellanina
+"""
+
+# QMB 3311
+# Isabella Nina
+# January 13, 2025
+
+
+movies = 5
+movies = movies * 2
+movies
+movies = movies + 5
+movies
+movies = movies * 50
+movies
+
+birthday = movies + 1749
+birthday
+year = birthday + 25
+year
+year = year - 2004
+year
